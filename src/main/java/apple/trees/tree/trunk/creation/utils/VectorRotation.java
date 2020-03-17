4e5a4fa6cacd5c92ff0f50ae49e87e4a1e53f12e
amp7368 <41219847+amp7368@users.noreply.github.com>
2020-03-16 23:40:48 -0400

    fixed the bug in VectorRotation where i did a dot product with the first row twice

--- src/main/java/apple/trees/tree/trunk/creation/utils/VectorRotation.java
+++ src/main/java/apple/trees/tree/trunk/creation/utils/VectorRotation.java
@@ -6,7 +6,7 @@ import com.sun.javafx.geom.Vec3f;
 
 public class VectorRotation {
 
-    public static Vec3d rotate(double xTheta, double yTheta, double zTheta, Vec3d vectorRaw) {
+    public static Vec3d rotate(double zTheta, double yTheta, double xTheta, Vec3d vectorRaw) {
         // convert the Vec3d to a Vec3f to do dot products later
         Vec3f vector = new Vec3f((float) vectorRaw.x, (float) vectorRaw.y, (float) vectorRaw.z);
 
@@ -18,12 +18,24 @@ public class VectorRotation {
         Matrix3f matrix = new Matrix3f();
 
         //refresher m01 is row 0, col 1
+//        matrix.m00 = (float) (Math.cos(zTheta) * Math.cos(yTheta));
+//        matrix.m01 = (float) (Math.cos(zTheta) * Math.sin(yTheta) * Math.sin(xTheta) - Math.sin(zTheta) * Math.cos(xTheta));
+//        matrix.m02 = (float) (Math.cos(zTheta) * Math.sin(yTheta) * Math.cos(xTheta) + Math.sin(zTheta) * Math.sin(xTheta));
+//        matrix.m10 = (float) (Math.sin(zTheta) * Math.cos(yTheta));
+//        matrix.m11 = (float) (Math.sin(zTheta) * Math.sin(yTheta) * Math.sin(xTheta) + Math.cos(zTheta) * Math.cos(xTheta));
+//        matrix.m12 = (float) (Math.sin(zTheta) * Math.sin(yTheta) * Math.cos(xTheta) - Math.cos(zTheta) * Math.sin(xTheta));
+//        matrix.m20 = (float) (-Math.sin(yTheta));
+//        matrix.m21 = (float) (Math.cos(yTheta) * Math.sin(xTheta));
+//        matrix.m22 = (float) (Math.cos(yTheta) * Math.cos(xTheta));
+//      same things ^v^v^v...
         matrix.m00 = (float) (Math.cos(zTheta) * Math.cos(yTheta));
-        matrix.m01 = (float) (Math.cos(zTheta) * Math.sin(yTheta) * Math.sin(xTheta) - Math.sin(zTheta) * Math.cos(xTheta));
-        matrix.m02 = (float) (Math.cos(zTheta) * Math.sin(yTheta) * Math.cos(xTheta) + Math.sin(zTheta) * Math.sin(xTheta));
-        matrix.m10 = (float) (Math.sin(zTheta) * Math.cos(yTheta));
-        matrix.m11 = (float) (Math.sin(zTheta) * Math.sin(yTheta) * Math.sin(xTheta) + Math.cos(zTheta) * Math.cos(xTheta));
-        matrix.m12 = (float) (Math.sin(zTheta) * Math.sin(yTheta) * Math.cos(xTheta) - Math.cos(zTheta) * Math.sin(xTheta));
+        matrix.m01 = (float) (Math.cos(zTheta) * Math.sin(yTheta) * Math.sin(xTheta) - Math.cos(xTheta) * Math.sin(zTheta));
+        matrix.m02 = (float) (Math.sin(zTheta) * Math.sin(xTheta) + Math.cos(zTheta) * Math.cos(xTheta) * Math.sin(yTheta));
+
+        matrix.m10 = (float) (Math.cos(yTheta) * Math.sin(zTheta));
+        matrix.m11 = (float) (Math.cos(zTheta) * Math.cos(xTheta) + Math.sin(zTheta) * Math.sin(yTheta) * Math.sin(xTheta));
+        matrix.m12 = (float) (Math.cos(xTheta) * Math.sin(zTheta) * Math.sin(yTheta) - Math.cos(zTheta) * Math.sin(xTheta));
+
         matrix.m20 = (float) (-Math.sin(yTheta));
         matrix.m21 = (float) (Math.cos(yTheta) * Math.sin(xTheta));
         matrix.m22 = (float) (Math.cos(yTheta) * Math.cos(xTheta));
@@ -38,9 +50,9 @@ public class VectorRotation {
         matrix.getRow(1, row2);
         double secondEntry = row2.dot(vector);
 
-        // dot product of row 1 with vector makes third entry
+        // dot product of row 3 with vector makes third entry
         Vec3f row3 = new Vec3f();
-        matrix.getRow(0, row3);
+        matrix.getRow(2, row3);
         double thirdEntry = row3.dot(vector);
 
         // return the rotated vector
