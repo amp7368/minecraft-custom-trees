2dabda4af9153769137642b16ddb621268c093ac
amp7368 <47309189+amp7368@users.noreply.github.com>
2020-03-17 15:04:52 -0400

    Revert "Cleaning constants"

--- src/main/java/apple/trees/TreeMain.java
+++ src/main/java/apple/trees/TreeMain.java
@@ -1,14 +1,4 @@
 package apple.trees;
 
-import apple.trees.commands.PlaceTreeCommand;
-import apple.trees.tree.trunk.creation.BaseTrunk;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class TreeMain extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        BaseTrunk.initialize(this);
-
-        new PlaceTreeCommand(this);
-    }
+public class TreeMain {
 }
